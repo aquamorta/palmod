4df1ecc42a0162d56e22c8d79c8853d542935332
Bernd Breitenbach <bernd@fiveoclock.de>
2019-02-01 12:45:51 +0100

    work saved

--- pypal.py
+++ pypal.py
@@ -12,50 +12,87 @@
 import argparse
 import re
 
-class Position(object):
+class State(object):
 
     def __init__(self):
         self.relTo={'X':0,'Y':0,'Z':0,'E':0}
-        self.pos={'X':0,'Y':0,'Z':0,'E':0}
+        self.state={'X':0,'Y':0,'Z':0,'E':0}
         self.absolute=True
 
     def move(self,axis,value):
         if self.absolute:
-            self.pos[axis]=value
+            self.state[axis]=value
         else:
-            self.pos[axis]=self.pos.get(axis,0)+value
+            self.state[axis]=self.state.get(axis,0)+value
     
-    def setRef(self,axis,value): 
+    def setRef(self,axis,value):
         pass
-    
+
+
+class Command(object):
+    MOVE_PAT=r'(\s+([XYZEF])%s)'%Command.FLOAT_PAT
+    FLOAT_PAT=r'-?(\d+\.?\d*|\.\d+)'
+    AXIS_PAT=r'(\s+[XYZE])?'
+
+    def process(self):
+        pass
+
+class MoveCmd(Command):
+    PATTERN=re.compile(r'G0*[01]%s%s?%s?%s?%s?'%(Command.MOVE_PAT,Command.MOVE_PAT,Command.MOVE_PAT,Command.MOVE_PAT,Command.MOVE_PAT))
+
+    def process(self,line,processor):
+        m=self.PATTERN.match(line)
+        pos=processor.state
+        if m:
+            i=1
+            while i<len(m) and m.group(i):
+                pos.move(m.group(i),m.group(i+1))
+                i+=3
+        return m!=None    
+        
+class HomeCmd(Command):
+    PATTERN=re.compile(r'G28%s%s%s%s'%(Command.AXIS_PAT,Command.AXIS_PAT,Command.AXIS_PAT,Command.AXIS_PAT))
+
+    def process(self,line,processor):
+        m=self.PATTERN.match(line)
+        pos=processor.state
+        if m:
+            i=1
+            while i<len(m) and m.group(i):
+                pos.setRef(m.group(i),0)
+                i+=1
+        return m!=None    
+
+class SetPosCmd(Command):
+    PATTERN=re.compile(r'G92%s%s?%s?%s?'%(Command.MOVE_PAT,Command.MOVE_PAT,Command.MOVE_PAT,Command.MOVE_PAT))
+
+    def process(self,line,processor):
+        m=self.PATTERN.match(line)
+        pos=processor.state
+        if m:
+            i=1
+            while i<len(m) and m.group(i):
+                pos.setRef(m.group(i),0)
+                i+=1
+        return m!=None    
+        
 
 class Processor(object):
-    COMMENT_PRE=r'\s*;\s*'
-    INT_PAT = r'*-?\d+'
     FLOAT_PAT=r'-?(\d+\.?\d*|\.\d+)'
+    COMMENT_PRE=r'\s*;\s*'
     COLOR_CHANGE=re.compile(r'^%stoolchange'%COMMENT_PRE)
     COLOR_CHANGE_END=re.compile(r'^%sendchroma'%COMMENT_PRE)
     EXTRUDER_PAT=re.compile(r'^(G0?1\s+E%s)\s+F2400\s+'%FLOAT_PAT)
     EXTRUDER_REP='%s F200\r\n'
-    MOVE_PAT=r'(\s+([XYZEF])%s)'%FLOAT_PAT
-
-    POS_CMD=re.compile(r'G0*[01]%s%s?%s?%s?%s?'%(MOVE_PAT,MOVE_PAT,MOVE_PAT,MOVE_PAT,MOVE_PAT))
 
     DEFAULT='D'
-    PROCESS='C'
-    
-
-    FSM={
-        (DEFAULT,POS_CMD):Processor.
-        'CHANGE'
-        }
-
+    CHANGE='C'
 
     def __init__(self,args):
         self.args=args
         self.func=self._scan
         self.lineNo=0
-        self.pos=Position()
+        self.state=State()
         
     def _scan(self,line):
         self.ofd.write(line)
@@ -73,11 +110,6 @@ class Processor(object):
             if self.COLOR_CHANGE_END.match(line):
                 self.func=self._scan
 
-    def move(self,matches):
-        i=1
-        while i<len(matches) and matches.group(i):
-            self.pos.move(matches.group(i),matches.group(i+1))
-            i+=3
 
     def home(self,matches):
         pass
@@ -87,9 +119,6 @@ class Processor(object):
             with open(self.args.input,'r') as fd:
                 for line in fd.readlines():
                     self.lineNo+=1
-                    m=self.POS_CMD.match(line)
-                    if m:
-                       self.move(m) 
                     self.func(line)
 
 if __name__ == '__main__':
